c0e931d04383788a101c5d4d6c2eb2957a3f688c
gabu <gabu@pop-os.localdomain>
2023-01-22 16:25:56 -0300

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    OPEN_AI_API_KEY: process.env.OPEN_AI_API_KEY,
+    VERCEL_URL: process.env.VERCEL_URL,
+  },
 }
 
 module.exports = nextConfig
